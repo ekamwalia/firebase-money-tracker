b8dca21a4dc789f25515c06a29f57d13b48f8076
pranavtharoor <pranav.tharoor@gmail.com>
2018-09-23 02:43:44 +0530

    Modified UI

--- final-code/functions/index.js
+++ final-code/functions/index.js
@@ -1,13 +1,16 @@
 const functions = require('firebase-functions');
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 // Adds a message that welcomes new users into the chat.
 exports.addWalletRecord = functions.auth.user().onCreate(async user => {
-    console.log('A new user signed in for the first time.');
+  console.log('A new user signed in for the first time.');
 
-    await admin.database().ref('/wallet/' + user.uid).set({
-        balance: 0
+  await admin
+    .database()
+    .ref('/wallet/' + user.uid)
+    .set({
+      balance: 0
     });
-    console.log('Wallet record created');
+  console.log('Wallet record created');
 });
